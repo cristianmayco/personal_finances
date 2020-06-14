f8383c9ff678a825d516da18e2a3a4045510619c
cristianmayco <cristianmayco@gmail.com>
2020-06-14 18:38:58 -0300

    Added bill admin pages

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core import models
 
-# Register your models here.
+
+@admin.register(models.Bill)
+class BillAdmin(admin.ModelAdmin):
+    ordering = ['create']
+    list_display = ['type', 'description', 'due_date', 'payday', 'active', 'amount']
+    list_filter = ['type', 'due_date', 'payday', 'active']
--- core/models.py
+++ core/models.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -18,14 +17,15 @@ class Bill(models.Model):
     )
     description = models.TextField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
+    create = models.DateField(auto_now_add=True)
     due_date = models.DateField(blank=False)
-    payday = models.DateField(null=True)
+    payday = models.DateField(null=True, blank=True)
     active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.type
 
     def save(self, *args, **kwargs):
-        if self.payday is None and self.active is False:
-            raise ValueError
+        if self.payday is None or self.payday == '' and self.active is False:
+            raise ValueError('Inactive payments must have payment date')
         return super(Bill, self).save(*args, **kwargs)
