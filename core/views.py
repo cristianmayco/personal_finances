017ae256d2b1df816530ced749412ed9d5f07f62
cristianmayco <cristianmayco@gmail.com>
2020-06-15 15:15:06 -0300

    Added paginator

--- core/views.py
+++ core/views.py
@@ -6,11 +6,8 @@ from .models import Bill
 class IndexView(ListView):
     template_name = 'index.html'
     model = Bill
-
-    def get_context_data(self, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['bills'] = Bill.objects.order_by('-id')
-        return context
+    paginate_by = 5
+    ordering = ['-id']
 
 
 class BillCreateView(CreateView):
